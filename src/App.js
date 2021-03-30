6d6a0ab4b5d68612ddc9a0abda5993c777a82f3f
IsmaelJdz7 <ismael_br7@hotmail.com>
2021-03-29 23:12:48 -0600

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import signInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
-  const [currentUser, setCurrentUser] = useState(null);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-  useEffect(() => {
-    auth.onAuthStateChanged(async userAuth => {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
           });
         });
       }
-      setCurrentUser({ userAuth });
-    });
-    return () => {
-      setCurrentUser(null);
-    };
-  }, []);
 
-  return (
-    <>
-      {console.log(currentUser)}
-      <Header currentUser={currentUser} />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={signInAndSignUp} />
-      </Switch>
-    </>
-  );
+      this.setState({ currentUser: userAuth });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -13,13 +13,17 @@ class SignIn extends React.Component {
     password: ''
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit = async evt => {
+    evt.preventDefault();
 
-    this.setState({
-      email: '',
-      password: ''
-    });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = e => {
