cc98a3fba1eef9ab0f6775673be931f8e085a1fd
Ismael Barrios R <ismael_br7@hotmail.com>
2021-05-20 21:38:13 -0500

    Created cart-item component and connected our cart drowndown with cartItem

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,6 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToAdd
+    cartItem => cartItem.id === cartItemToAdd.id
   );
 
   if (existingCartItem) {
